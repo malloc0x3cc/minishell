8ee21db87fcd12fe3a76130675e0f002f29f57f7
MDW <32233686+malloc0x3cc@users.noreply.github.com>
2026-06-05 00:01:39 +0200

    prompt working

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: madelwau <madelwau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 13:00:08 by madelwau          #+#    #+#             */
-/*   Updated: 2026/06/04 13:00:49 by madelwau         ###   ########.fr       */
+/*   Updated: 2026/06/05 00:00:52 by madelwau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,14 @@
 
 int	main(int ac, char **av)
 {
+	char	*test;
+
 	(void)ac;
 	(void)av;
+	while (1)
+	{
+		test = readline(PROMPT);
+		printf("%s\n", test);
+	}
 	return (0);
 }
