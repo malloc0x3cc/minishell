4d7e09f6ce3422ff063bf922d109fe3258b2f46d
MDW <32233686+malloc0x3cc@users.noreply.github.com>
2026-06-05 16:03:32 +0200

    basic prompt with history

--- minishell.c
+++ minishell.c
@@ -6,22 +6,28 @@
 /*   By: madelwau <madelwau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 13:00:08 by madelwau          #+#    #+#             */
-/*   Updated: 2026/06/05 00:00:52 by madelwau         ###   ########.fr       */
+/*   Updated: 2026/06/05 16:01:52 by madelwau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int ac, char **av)
+int	main(void)
 {
-	char	*test;
+	char	*input;
 
-	(void)ac;
-	(void)av;
 	while (1)
 	{
-		test = readline(PROMPT);
-		printf("%s\n", test);
+		input = readline(PROMPT);
+		if (!input)
+			break ;
+		if (*input)
+		{
+			add_history(input);
+			printf("%s\n", input);
+		}
+		free(input);
 	}
+	rl_clear_history();
 	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -6,16 +6,24 @@
 /*   By: madelwau <madelwau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 13:01:08 by madelwau          #+#    #+#             */
-/*   Updated: 2026/06/04 23:42:32 by madelwau         ###   ########.fr       */
+/*   Updated: 2026/06/05 16:00:59 by madelwau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <stdio.h>
 # include <readline/readline.h>
+# include <readline/history.h>
 # include "libft/inc/libft.h"
 
 # define PROMPT "minishell> "
+
+typedef struct s_cmd
+{
+	char			**args;
+	struct s_cmd	*next;
+}	t_cmd;
 
 #endif
